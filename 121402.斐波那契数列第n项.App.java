a7dc4f6dd03a5225a402fa5203e8768ebee91522
Threeyearsold <74776449+Threeyearsold@users.noreply.github.com>
2020-12-14 17:03:11 +0800

    Create 121402.斐波那契数列第n项.App.java
    用户每次输入一个int型数字的项数n，都可以输入斐波那契数列的第n项的值。本程序取值区间：（1<=n<=40）。

---
+++ 121402.斐波那契数列第n项.App.java
@@ -0,0 +1,29 @@
+package pac;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Dinxiang {
+	public static void main(String[] args) {
+		 Scanner cin=new Scanner(System.in);
+	        int n=51;
+	        BigInteger[] fib=new BigInteger[n];
+	        fib[1]=BigInteger.valueOf(1);
+	        fib[2]=BigInteger.valueOf(1);
+	        int i,j;
+	 
+	        for(i=3;i<n;i++)
+	        {
+	           
+	            fib[i]=fib[i-1].add(fib[i-2]);
+	        }
+	 
+	        int m;
+	        while(cin.hasNext())
+	        {
+	            m=cin.nextInt();
+	            System.out.println(fib[m]);
+	 
+	        }
+	}
+}
